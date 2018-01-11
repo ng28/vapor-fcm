e67e41e8d56c0dbc18870efd9df88b63015ce3f5
Ng <ng@ng-flocks.local>
2018-01-11 14:52:44 +0530

    - Added separate RawRepresentable to handle multiple device tokens.

--- Sources/VaporFCM/DeviceTokens.swift
+++ Sources/VaporFCM/DeviceTokens.swift
@@ -5,4 +5,29 @@
 //  Created by Ng on 1/11/18.
 //
 
-import Foundation
+/// This represents one or more FCM device tokens
+public struct DeviceTokens: RawRepresentable {
+    public var rawValue: String
+    public typealias RawValue = String
+    public let rawValues: Array<String>
+    
+    public init?(rawValue: String) {
+        self.rawValue = ""
+        self.rawValues = Array<String>()
+    }
+    
+    public init(_ rawValues: Array<String>) {
+        self.rawValue = ""
+        self.rawValues = rawValues
+    }
+}
+
+extension DeviceTokens: Targetable {
+    public var targetKey: String {
+        return "registration_ids"
+    }
+    
+    public var targetValue: String {
+        return rawValues.joined(separator: ",")
+    }
+}
